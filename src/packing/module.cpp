44bba342f02ccb92d99b0a3ab6cbd37b58cc013d
Malcolm Ramsay <malramsay64@gmail.com>
2018-12-19 16:27:45 +1100

    Use packing module name

--- src/packing/module.cpp
+++ src/packing/module.cpp
@@ -6,6 +6,20 @@
  */
 
 #include "random.h"
-#include <pybind11/pybind11>
+#include <pybind11/pybind11.h>
 
-PYBIND11_MODULE(_packing, m) { export_fluke(m); }
+PYBIND11_MODULE(packing, m) {
+  m.doc() = R"pbdoc(
+      Pybind11 test
+      -------------
+      )pbdoc";
+  m.def("fluke", &fluke, R"pbdoc(
+      Generate a random number
+      )pbdoc");
+
+#ifdef VERSION_INFO
+  m.attr("__version__") = VERSION_INFO;
+#else
+  m.attr("__version__") = "dev";
+#endif
+}
