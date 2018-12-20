3f055d988bd264987c32dc1fe693edbb9dcd03bb
Malcolm Ramsay <malramsay64@gmail.com>
2018-12-21 08:50:43 +1100

    MAINT Break basis into it's own file
    The basis class and it's subclasses are rather complicated functionality
    with no real relation to the shapes class, so I have moved this to it's
    own file to make it simpler to mange the functionality.

---
+++ src/packing/basis.cpp
@@ -0,0 +1,99 @@
+/*
+ * basis.cpp
+ * Copyright (C) 2018 Malcolm Ramsay <malramsay64@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "basis.h"
+
+namespace py = pybind11;
+
+double Basis::value_range() const { return this->max_val - this->min_val; };
+
+void Basis::set_value(double new_value) {
+  if (new_value < this->min_val) {
+    new_value = this->min_val;
+  } else if (new_value > this->max_val) {
+    new_value = this->max_val;
+  }
+  this->value_previous = this->value;
+  this->value = new_value;
+}
+double Basis::get_value() const { return this->value; };
+
+void Basis::reset_value() { this->value = this->value_previous; };
+
+double Basis::get_random_value(double kT) const {
+  return this->value + this->step_size * this->value_range() * (fluke() - 0.5);
+}
+
+double CellLengthBasis::get_random_value(double kT) const {
+  return this->get_value() + (1.0 + std::min(3.0 * kT, 0.1) * (fluke() - 0.5));
+}
+
+double CellAngleBasis::get_random_value(double kT) const {
+  return this->value + this->step_size * this->value_range() * (fluke() - 0.5);
+}
+
+void CellAngleBasis::set_value(double new_value) {
+  if (new_value < this->min_val) {
+    new_value = this->min_val;
+  } else if (new_value > this->max_val) {
+    new_value = this->max_val;
+  }
+  this->value_previous = this->value;
+  this->value = new_value;
+  this->update_cell_lengths();
+};
+
+void CellAngleBasis::reset_value() {
+  this->value = this->value_previous;
+  this->reset_cell_lengths();
+};
+
+void CellAngleBasis::update_cell_lengths() {
+  this->cell_x_len->set_value(
+      this->cell_x_len->get_value() *
+      sqrt(sin(this->value_previous) / sin(this->value)));
+  this->cell_y_len->set_value(
+      this->cell_y_len->get_value() *
+      sqrt(sin(this->value_previous) / sin(this->value)));
+};
+
+void CellAngleBasis::reset_cell_lengths() {
+  this->cell_x_len->reset_value();
+  this->cell_y_len->reset_value();
+};
+
+double MirrorBasis::get_random_value(double kT) const {
+  if ((this->mirrors % 2 == 0) && (fluke() < 0.5)) {
+    /* turn it 90 degrees to switch the x and y mirror planes */
+    if (this->value < M_PI * 3.0 / 4.0) {
+      return this->value + PI / this->mirrors;
+    }
+    return this->value - PI / this->mirrors;
+  }
+  /* turn it 180 degrees so that all mirror planes are preserved */
+  return positive_modulo(this->value + PI, 2 * PI);
+}
+
+double FlipBasis::get_random_value(double kT) const {
+  return rand() % occupied_sites->size();
+}
+
+void FlipBasis::set_value(double new_value) {
+  this->value_previous = static_cast<int>(std::round(new_value));
+  occupied_sites->at(this->value_previous).flip_site ^= 1;
+}
+
+void FlipBasis::reset_value() {
+  if (this->value_previous == -1) {
+    return;
+  }
+  occupied_sites->at(this->value_previous).flip_site ^= 1;
+  // Only allow the reset_value to occur once
+  this->value_previous = -1;
+}
+
+void export_Basis(py::module& m) {}
--- src/packing/shapes.h
+++ src/packing/shapes.h
@@ -1,5 +1,5 @@
+#include "basis.h"
 #include "math.h"
-#include "random.h"
 #include <algorithm>
 #include <cmath>
 #include <fstream>
@@ -11,98 +11,6 @@
 
 #ifndef SHAPES_H
 #define SHAPES_H
-
-// Forward declaration of Site class
-class Site;
-
-class Basis {
-protected:
-  double value_previous;
-  double value;
-
-public:
-  double min_val;
-  double max_val;
-  double step_size = 1;
-
-  Basis(double value, double min_val, double max_val, double step_size)
-      : value(value), min_val(min_val), max_val(max_val), step_size(step_size){};
-  Basis(double value, double min_val, double max_val)
-      : Basis(value, min_val, max_val, 1){};
-
-  double value_range() const;
-  double get_value() const;
-  void set_value(double new_value);
-  void reset_value();
-  double get_random_value(double kT) const;
-};
-
-class CellLengthBasis : public Basis {
-public:
-  double step_size;
-
-  CellLengthBasis(double value, double min_val, double max_val, double step_size)
-      : Basis(value, min_val, max_val), step_size(step_size){};
-
-  double get_random_value(double kT) const;
-};
-
-class CellAngleBasis : public Basis {
-private:
-  void update_cell_lengths();
-  void reset_cell_lengths();
-
-public:
-  double step_size;
-  Basis* cell_x_len;
-  Basis* cell_y_len;
-
-  CellAngleBasis(
-      double value,
-      double min_val,
-      double max_val,
-      double step_size,
-      Basis* cell_x_len,
-      Basis* cell_y_len)
-      : Basis(value, min_val, max_val), step_size(step_size), cell_x_len(cell_x_len),
-        cell_y_len(cell_y_len){};
-
-  void set_value(double new_value);
-  void reset_value();
-  double get_random_value(double kT) const;
-};
-
-class FixedBasis : public Basis {
-public:
-  FixedBasis(double value) : Basis(value, value, value){};
-
-  void set_value(double new_value){};
-  void reset_value(){};
-};
-
-class MirrorBasis : public Basis {
-public:
-  int mirrors;
-
-  MirrorBasis(double value, double min_val, double max_val, int mirrors)
-      : Basis(value, min_val, max_val), mirrors(mirrors){};
-
-  double get_random_value(double kT) const;
-};
-
-class FlipBasis : public Basis {
-private:
-  std::vector<Site>* occupied_sites;
-  int value_previous;
-
-public:
-  FlipBasis(std::vector<Site>* occupied_sites)
-      : Basis(0, 0, occupied_sites->size()), occupied_sites(occupied_sites){};
-
-  double get_random_value(double kT) const;
-  void set_value(double new_value);
-  void reset_value();
-};
 
 struct Cell {
   Basis* x_len;
