909012ab78518312e3f913b607378efc250855db
Malcolm Ramsay <malramsay64@gmail.com>
2018-12-25 09:19:59 +1100

    DOC Documentation of geometry functions

--- src/packing/geometry.cpp
+++ src/packing/geometry.cpp
@@ -12,19 +12,33 @@
 
 namespace py = pybind11;
 
-// To find orientation of ordered triplet (a, b, c).
-// The function returns following values
-// 0 --> a, b and c are colinear
-// 1 --> Clockwise
-// -1 --> Counterclockwise
+/** Find the orientation of an ordered triplet of points; a, b, and c.
+ *
+ * This is adapted from a post which has additional details on the algorithm.
+ * https://www.geeksforgeeks.org/orientation-3-ordered-points/
+ *
+ * \param a First point in ordered triplet
+ * \param b Second point in ordered triplet
+ * \param c Third point in ordered triplet
+ *
+ * \return Integer describing the orietation of the triplet.
+ *          - 0 --> a, b, and c are colinear (on the same line)
+ *          - 1 --> A clockwise orientation
+ *          - -1 --> Anti-Clockwise orientation
+ */
 int triplet_orientation(const Vect2& a, const Vect2& b, const Vect2& c) {
-  // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
-  // for details of below formula.
   return sign((b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y));
 }
 
-// Given three colinear points a, b, c, the function checks if
-// point b lies on line segment 'ac'
+/** Checks whether the point b lies on the line segment ac.
+ *
+ * This function assumes the points are on the same line, so should be used after
+ * triplet_orientation has returned the value 0 indicating co-linearity.
+ *
+ * \param a The first point of the line segment
+ * \param b The point we are checking is on the line segment
+ * \paarm c The second point of the line segment
+ */
 bool on_segment(const Vect2& a, const Vect2& b, const Vect2& c) {
   if (b.x <= std::max(a.x, c.x) && b.x >= std::min(a.x, c.x) &&
       b.y <= std::max(a.y, c.y) && b.y >= std::min(a.y, c.y)) {
@@ -35,10 +49,12 @@ bool on_segment(const Vect2& a, const Vect2& b, const Vect2& c) {
 }
 
 /** Evaluate whether the line segment A1B1 crosses the line segment A2B2
+ *
  * \param A1 The first point of the first line segment
  * \param B1 The second point of the first line segment
  * \param A2 The first point of the second line segment
  * \param B2 The second point of the second line segment
+ *
  * \returns bool indicating whether the line segments cross at some point.
  */
 bool segments_cross(
@@ -63,15 +79,15 @@ bool segments_cross(
   if (o1 == 0 && on_segment(A1, A2, B1))
     return true;
 
-  // p1, q1 and q2 are colinear and q2 lies on segment A1B1
+  // A1, B1 and B2 are colinear and B2 lies on segment A1B1
   if (o2 == 0 && on_segment(A1, B2, B1))
     return true;
 
-  // p2, q2 and p1 are colinear and p1 lies on segment A2B2
+  // A2, B2 and A1 are colinear and A1 lies on segment A2B2
   if (o3 == 0 && on_segment(A2, A1, B2))
     return true;
 
-  // p2, q2 and q1 are colinear and q1 lies on segment A2B2
+  // A2, B2 and A1 are colinear and B1 lies on segment A2B2
   if (o4 == 0 && on_segment(A2, B1, B2))
     return true;
 
